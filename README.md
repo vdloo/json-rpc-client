eb3777b4d4ff741a6fb85e7a826fce2585753f01
Rick van de Loo <rickvandeloo@gmail.com>
2016-04-03 15:48:33 +0200

    provide json-rpc-client and update README

--- README.md
+++ README.md
@@ -2,6 +2,30 @@
 
 JSON-RPC client in racket
 
+## Installation
+
+```
+git clone https://github.com/vdloo/json-rpc-client && \
+cd json-rpc-client && \
+raco pkg install
+```
+
+## Usage
+
+Require the package, create the payload as a hash and send it to a URL.
+
+```
+(require json-rpc-client)
+
+(define payload 
+  (hasheq
+    'method "JSONRPC.Introspect" 
+    'jsonrpc "2.0" 
+    'id 1))
+
+(json-rpc-client "http://127.0.0.1:8088/jsonrpc" payload) 
+```
+
 ## Development
 
 Running the tests once:
--- main.rkt
+++ main.rkt
@@ -1,19 +1,5 @@
 #!/usr/bin/env racket
 #lang racket/base
 
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco doc <<name>>
-
-;; Code here
-
-(module+ main
-  ;; Main entry point, executed when run with the `racket` executable or DrRacket.
-  (require "json-rpc-client/main.rkt")
-  )
+(require "json-rpc-client/main.rkt")
+(provide json-rpc-client)
