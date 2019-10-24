c7fba3b4a6bc6f6d2354739ea0fd5eee276161d7
stellarpower <stellarpower@googlemail.com>
2019-10-24 04:16:16 +0100

    Make Explicit in Loop
    I don't speak Racket but looks like the tests are commented-out and not doing anything... anyway, I hadn't realised this was in an endless loop so this makes it explicit. Trying to test after my other commit with mock.

--- runtests.sh
+++ runtests.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 run_tests() {
-    echo "Running tests.."
+    echo "Running tests.. (iteration $1)"
     racket tests/main.rkt
 }
 
 if [ $1 -eq '-1' ]; then
     run_tests
 else
+    iter=1
     while :; do
         clear
-	run_tests
+	run_tests $iter
         sleep 10
+        iter=$(($iter + 1))
     done
 fi;
